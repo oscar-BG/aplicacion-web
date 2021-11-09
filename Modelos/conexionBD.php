41b9c7d3181327f1d584300dcdde825a017cfccf
Oscar Bautista <oscar04262000@gmail.com>
2021-11-09 12:26:42 -0600

    Clase de alert

--- Modelos/conexionBD.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ConexionBD{
-    static public function cBD(){
-        // Base de datos = tallerbd
-        $bd = new PDO("mysql:host=localhost;dbname=tallerbd","root","");
-        return $bd;
-    }
-}
-?>
